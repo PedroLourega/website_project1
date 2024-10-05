5340464bef50543ac69508d69098f481f1b0294c
Pedro Lourega <pedrolourega1@gmail.com>
2024-10-04 21:02:47 -0300

    update-website4
    update testimonials section and gallery section. update4

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ menuOpenButton.addEventListener("click", () => {
 
     // Close menu when the close buttin is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+    //Responsive breakpoints
+    breakpoints:{
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        }
+    }
+  });
